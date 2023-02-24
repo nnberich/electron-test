1da68a5902c0733bfa02e3333b81a7828ae67253
caoningxia <caoningxia@mediinfo.com.cn>
2023-02-24 10:39:33 +0800

    发布

--- my-electron-app/forge.config.js
+++ my-electron-app/forge.config.js
@@ -17,5 +17,18 @@ module.exports = {
         icon: '/path/to/icon.ico',
       }
     }
-  ]
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'nnberich',
+          name: 'electron-test',
+        },
+        prerelease: false,
+        draft: true,
+      },
+    },
+  ],
 };
